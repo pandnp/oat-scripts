999f095308b3e258a2205988249a85cf500ac609
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2013-09-18 14:10:38 -0500

    added glassfish specific scripts

---
+++ glassfish.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+OPEN_ATTESTATION=/home/fin/OpenAttestation
+OAT_JARS=$OPEN_ATTESTATION/trust-agent/TrustAgent/target/jars
+OAT_LIB=$OPEN_ATTESTATION/trust-agent/HisPrivacyCAWebServices2/target/WEB-INF/lib
+PORTALS_LIB=$OPEN_ATTESTATION/portals/TrustDashBoard/target/TrustDashBoard-1.2-SNAPSHOT/WEB-INF/lib
+GLASSFISH_HOME=
+
+if [ ! -e $GLASSFISH_HOME/modules ]; then
+  mkdir $GLASSFISH_HOME/modules
+fi
+
+# jackson-core-asl-1.9.11.jar   
+# jackson-jaxrs-1.9.11.jar
+# jackson-mapper-asl-1.9.11.jar
+# jackson-xc-1.9.11.jar
+cp $OAT_JARS/jackson*.jar $GLASSFISH_HOME/modules
+cp $PORTALS_LIB/jackson-xc*.jar $GLASSFISH_HOME/modules
+
+#javax.servlet-api.jar
+#javax.annotation-3.1.1.jar
+#javax.annotation.jar
+cp $OAT_JARS/javax.servlet*.jar $GLASSFISH_HOME/modules
+
+#jaxws-api.jar
+#jaxws-rt.jar
+#jaxws-tools.jar
+cp $OAT_LIB/jaxws*.jar $GLASSFISH_HOME/modules
+
+#jaxb-api-osgi.jar
+#jaxb-impl-2.1.9.jar
+#jaxb-api-2.1.jar
+#jaxb-xjc.jar
+cp $OAT_LIB/jaxb*.jar $GLASSFISH_HOME/modules
+
+#jsr181-api.jar
+#jsr311-api-1.1.1.jar
+cp $OAT_LIB/jsr181*.jar $GLASSFISH_HOME/modules
+cp $PORTALS_LIB/jsr311*.jar $GLASSFISH_HOME/modules
+
+#streambuffer.jar
+#stax-ex.jar
+#servlet-api.jar
+#mysql-connector-java-5.1.22.jar
+#webservices-api-osgi.jar
+cp $OAT_LIB/streambuffer*.jar $GLASSFISH_HOME/modules
+cp $OAT_LIB/stax*.jar $GLASSFISH_HOME/modules
+cp $OAT_LIB/servlet*.jar $GLASSFISH_HOME/modules
+cp $PORTALS_LIB/mysql-connector*.jar $GLASSFISH_HOME/modules
