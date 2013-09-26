9a0e240912154056ca1fc05cdbafaca7ee5dca1f
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2013-09-25 20:24:11 -0500

    moved GLASSFISH def

--- oat.sh
+++ oat.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 HOST=`/bin/hostname`
+GLASSFISH_HOME=/root/home/glasfish3/glassfish
 USER=fin
 IP=192.168.1.1
-OAT=/root/home
-GLASSFISH_HOME=/root/home/glasfish3/glassfish
+OAT=/root/home/OpenAttestation
 
-sh mysql.sh $OAT
-sh glassfish $OAT
-sh config.sh $HOST $IP
-sh aikqverify.sh $OAT
-sh keytool.sh $OAT $GLASSFISH_HOME $IP $USER
-sh deploy.sh $OAT $GLASSFISH_HOME
+sh mysql.sh "$OAT"
+sh glassfish "$OAT"
+sh config.sh "$HOST $IP"
+sh aikqverify.sh "$OAT"
+sh keytool.sh "$OAT" "$GLASSFISH_HOME" "$IP" "$USER"
+sh deploy.sh "$OAT" "$GLASSFISH_HOME"
